95ee7f113661e00cf30b53e278a966940cc59945
Kemal Hadimli <disq@users.noreply.github.com>
2023-07-28 13:51:05 +0100

    factory: clean up

--- cloudquery/sdk/scalar/scalar_factory.py
+++ cloudquery/sdk/scalar/scalar_factory.py
@@ -34,53 +34,41 @@ class ScalarFactory:
             return Uint(bitwidth=16)
         elif dt_id == pa.types.lib.Type_UINT8:
             return Uint(bitwidth=8)
-        elif dt_id == pa.types.lib.Type_BINARY:
+        elif (
+            dt_id == pa.types.lib.Type_BINARY
+            or dt_id == pa.types.lib.Type_LARGE_BINARY
+            or dt_id == pa.types.lib.Type_FIXED_SIZE_BINARY
+        ):
             return Binary()
         elif dt_id == pa.types.lib.Type_BOOL:
             return Bool()
-        elif dt_id == pa.types.lib.Type_DATE32:
-            return Date32()
         elif dt_id == pa.types.lib.Type_DATE64:
             return Date64()
-        # elif dt_id == pa.types.lib.Type_DECIMAL128:
-        #     return ()
+        elif dt_id == pa.types.lib.Type_DATE32:
+            return Date32()
         # elif dt_id == pa.types.lib.Type_DECIMAL256:
         #     return ()
-        # elif dt_id == pa.types.lib.Type_DENSE_UNION:
+        # elif dt_id == pa.types.lib.Type_DECIMAL128:
         #     return ()
         # elif dt_id == pa.types.lib.Type_DICTIONARY:
         #     return ()
-        elif dt_id == pa.types.lib.Type_DOUBLE:
-            return Float(bitwidth=64)
         # elif dt_id == pa.types.lib.Type_DURATION:
         #     return ()
-        elif dt_id == pa.types.lib.Type_FIXED_SIZE_BINARY:
-            return Binary()
-        # elif dt_id == pa.types.lib.Type_FIXED_SIZE_LIST:
-        #     return ()
+        elif dt_id == pa.types.lib.Type_DOUBLE:
+            return Float(bitwidth=64)
         elif dt_id == pa.types.lib.Type_FLOAT:
             return Float(bitwidth=32)
         elif dt_id == pa.types.lib.Type_HALF_FLOAT:
             return Float(bitwidth=16)
         # elif dt_id == pa.types.lib.Type_INTERVAL_MONTH_DAY_NANO:
         #     return ()
-        # elif dt_id == pa.types.lib.Type_LARGE_BINARY:
-        #     return ()
-        # elif dt_id == pa.types.lib.Type_LARGE_LIST:
-        #     return ()
-        elif dt_id == pa.types.lib.Type_LARGE_STRING:
-            return String()
-        # elif dt_id == pa.types.lib.Type_LIST:
-        #     return ()
+        # elif dt_id == pa.types.lib.Type_LIST or dt_id == pa.types.lib.Type_LARGE_LIST or dt_id == pa.types.lib.Type_FIXED_SIZE_LIST:
+        #     return List()
         # elif dt_id == pa.types.lib.Type_MAP:
         #     return ()
-        # elif dt_id == pa.types.lib.Type_NA:
-        #     return ()
-        # elif dt_id == pa.types.lib.Type_RUN_END_ENCODED:
-        #     return ()
-        # elif dt_id == pa.types.lib.Type_SPARSE_UNION:
-        #     return ()
-        elif dt_id == pa.types.lib.Type_STRING:
+        elif (
+            dt_id == pa.types.lib.Type_STRING or dt_id == pa.types.lib.Type_LARGE_STRING
+        ):
             return String()
         # elif dt_id == pa.types.lib.Type_STRUCT:
         #     return ()
