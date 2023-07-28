acab10862213f86d8328ec866da360dcec9eaff4
Kemal Hadimli <disq@users.noreply.github.com>
2023-07-28 13:03:52 +0100

    oops (fmt)

--- tests/scalar/timestamp.py
+++ tests/scalar/timestamp.py
@@ -8,11 +8,11 @@ import pandas as pd
     "input_value,expected_scalar",
     [
         (
-                datetime.strptime("2006-01-02T15:04:05", "%Y-%m-%dT%H:%M:%S"),
-                Timestamp(
-                    True,
-                    pd.to_datetime("2006-01-02 15:04:05"),
-                ),
+            datetime.strptime("2006-01-02T15:04:05", "%Y-%m-%dT%H:%M:%S"),
+            Timestamp(
+                True,
+                pd.to_datetime("2006-01-02 15:04:05"),
+            ),
         ),
         (
             "2006-01-02T15:04:05Z",
@@ -30,7 +30,7 @@ import pandas as pd
         ),
         (
             "2006-01-02 15:04:05.999999999 -0700",
-#            "2006-01-02 15:04:05.999999999 -0700 MST",
+            #            "2006-01-02 15:04:05.999999999 -0700 MST",
             Timestamp(
                 True,
                 pd.to_datetime("2006-01-02 15:04:05.999999999 -0700"),
@@ -44,16 +44,15 @@ import pandas as pd
             ),
         ),
         (
-                "2006-01-02 15:04:05.999999999Z",
-                Timestamp(
-                    True,
-                    pd.to_datetime("2006-01-02 15:04:05.999999999Z"),
-                ),
+            "2006-01-02 15:04:05.999999999Z",
+            Timestamp(
+                True,
+                pd.to_datetime("2006-01-02 15:04:05.999999999Z"),
+            ),
         ),
     ],
 )
 def test_timestamp_set(input_value, expected_scalar):
     b = Timestamp()
     b.set(input_value)
-    assert b == expected_scalar, f'{b} and {expected_scalar} are not equal'
-
+    assert b == expected_scalar, f"{b} and {expected_scalar} are not equal"
