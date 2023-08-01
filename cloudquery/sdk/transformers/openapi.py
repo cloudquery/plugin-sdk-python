e774e0c9f3d96343a124fe94f19ab6344b3fdf07
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-08-01 18:53:36 +0300

    feat: Add override_columns to openapi transformer

--- cloudquery/sdk/transformers/openapi.py
+++ cloudquery/sdk/transformers/openapi.py
@@ -23,12 +23,21 @@ def oapi_type_to_arrow_type(field) -> pa.DataType:
     else:
         return pa.string()
 
+def get_column_by_name(columns: List[Column], name: str) -> Column:
+    for column in columns:
+        if column.name == name:
+            return column
+    return None
 
-def oapi_definition_to_columns(definition: Dict) -> List[Column]:
+def oapi_definition_to_columns(definition: Dict, override_columns=[]) -> List[Column]:
     columns = []
     for key, value in definition["properties"].items():
         column_type = oapi_type_to_arrow_type(value)
-        columns.append(
-            Column(name=key, type=column_type, description=value.get("description"))
-        )
+        column = Column(name=key, type=column_type, description=value.get("description"))
+        override_column = get_column_by_name(override_columns, key)
+        if override_column is not None:
+            column.type = override_column.type
+            column.primary_key = override_column.primary_key
+            column.unique = override_column.unique
+        columns.append(column)
     return columns
