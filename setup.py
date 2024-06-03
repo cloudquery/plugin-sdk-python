373f8c49ec74bc0e0d0efd789c379ba23f1f68dd
Erez Rokah <erezrokah@users.noreply.github.com>
2024-06-03 15:01:08 +0300

    Revert "fix(deps): Update dependency python-dateutil to v2.9.0.post0 (#182)"
    This reverts commit b03fe3d832be634bae7756cc4ca61dd90249b3e4.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dependencies = [
     "protobuf==5.27.0",
     "pyarrow==15.0.2",
     "pytest==8.2.1",
-    "python-dateutil==2.9.0.post0",
+    "python-dateutil==2.8.2",
     "pytz==2024.1",
     "six==1.16.0",
     "structlog==23.3.0",
